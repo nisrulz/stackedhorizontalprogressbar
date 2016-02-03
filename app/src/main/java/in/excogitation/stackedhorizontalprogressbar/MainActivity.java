f70c1e2af0587ce19b81c9e945694e9737deb71a
Nishant Srivastava <nisrulz@gmail.com>
2016-02-03 17:57:23 +0530

    updated code to handle secondary progress and max value

--- app/src/main/java/in/excogitation/stackedhorizontalprogressbar/MainActivity.java
+++ app/src/main/java/in/excogitation/stackedhorizontalprogressbar/MainActivity.java
@@ -11,14 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int primary_pts = 4;
-        int secondary_pts = 3;
+        int primary_pts = 3;
+        int secondary_pts =6;
         int max = 10;
 
         StackedHorizontalProgressBar stackedHorizontalProgressBar;
         stackedHorizontalProgressBar = (StackedHorizontalProgressBar) findViewById(R.id.stackedhorizontalprogressbar);
         stackedHorizontalProgressBar.setMax(max);
         stackedHorizontalProgressBar.setProgress(primary_pts);
-        stackedHorizontalProgressBar.setSecondaryProgress(primary_pts + secondary_pts);
+        stackedHorizontalProgressBar.setSecondaryProgress(secondary_pts);
     }
 }
