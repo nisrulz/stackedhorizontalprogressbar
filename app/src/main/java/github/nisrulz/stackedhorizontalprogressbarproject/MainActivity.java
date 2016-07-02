47d4d18b79fe7e1106f6fab42f2dd93b5f2fe1de
Nishant Srivastava <nisrulz@gmail.com>
2016-07-02 17:43:46 +0530

    [*] updates - bumped up version to v1.0.2-3 - added support for api 24 - code reformatted

--- app/src/main/java/github/nisrulz/stackedhorizontalprogressbarproject/MainActivity.java
+++ app/src/main/java/github/nisrulz/stackedhorizontalprogressbarproject/MainActivity.java
@@ -18,24 +18,22 @@ package github.nisrulz.stackedhorizontalprogressbarproject;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
 import github.nisrulz.stackedhorizontalprogressbar.StackedHorizontalProgressBar;
-
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        int primary_pts = 3;
-        int secondary_pts = 6;
-        int max = 10;
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    int primary_pts = 3;
+    int secondary_pts = 6;
+    int max = 10;
 
-        StackedHorizontalProgressBar stackedHorizontalProgressBar;
-        stackedHorizontalProgressBar = (StackedHorizontalProgressBar) findViewById(R.id.stackedhorizontalprogressbar);
-        stackedHorizontalProgressBar.setMax(max);
-        stackedHorizontalProgressBar.setProgress(primary_pts);
-        stackedHorizontalProgressBar.setSecondaryProgress(secondary_pts);
-    }
+    StackedHorizontalProgressBar stackedHorizontalProgressBar;
+    stackedHorizontalProgressBar =
+        (StackedHorizontalProgressBar) findViewById(R.id.stackedhorizontalprogressbar);
+    stackedHorizontalProgressBar.setMax(max);
+    stackedHorizontalProgressBar.setProgress(primary_pts);
+    stackedHorizontalProgressBar.setSecondaryProgress(secondary_pts);
+  }
 }
--- lib/src/main/java/github/nisrulz/stackedhorizontalprogressbar/StackedHorizontalProgressBar.java
+++ lib/src/main/java/github/nisrulz/stackedhorizontalprogressbar/StackedHorizontalProgressBar.java
@@ -22,49 +22,43 @@ import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
 
-/**
- * @author Nishant Srivastava
- */
 public class StackedHorizontalProgressBar extends ProgressBar {
-    private Paint paint;
-    int primary_progress, max_value;
+  private Paint paint;
+  int primary_progress, max_value;
 
-    public StackedHorizontalProgressBar(Context context) {
-        super(context);
-        paint = new Paint();
-        paint.setColor(Color.BLACK);
-        primary_progress = 0;
-        max_value = 100;
-    }
+  public StackedHorizontalProgressBar(Context context) {
+    super(context);
+    paint = new Paint();
+    paint.setColor(Color.BLACK);
+    primary_progress = 0;
+    max_value = 100;
+  }
 
-    public StackedHorizontalProgressBar(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        paint = new Paint();
-        paint.setColor(Color.BLACK);
-        primary_progress = 0;
-        max_value = 100;
-    }
+  public StackedHorizontalProgressBar(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    paint = new Paint();
+    paint.setColor(Color.BLACK);
+    primary_progress = 0;
+    max_value = 100;
+  }
 
-    @Override
-    public synchronized void setMax(int max) {
-        this.max_value = max;
-        super.setMax(max);
-    }
+  @Override public synchronized void setMax(int max) {
+    this.max_value = max;
+    super.setMax(max);
+  }
 
-    @Override
-    public synchronized void setProgress(int progress) {
-        if (progress > max_value) {
-            progress = max_value;
-        }
-        this.primary_progress = progress;
-        super.setProgress(progress);
+  @Override public synchronized void setProgress(int progress) {
+    if (progress > max_value) {
+      progress = max_value;
     }
+    this.primary_progress = progress;
+    super.setProgress(progress);
+  }
 
-    @Override
-    public synchronized void setSecondaryProgress(int secondaryProgress) {
-        if ((primary_progress + secondaryProgress) > max_value) {
-            secondaryProgress = (max_value - primary_progress);
-        }
-        super.setSecondaryProgress(primary_progress + secondaryProgress);
+  @Override public synchronized void setSecondaryProgress(int secondaryProgress) {
+    if ((primary_progress + secondaryProgress) > max_value) {
+      secondaryProgress = (max_value - primary_progress);
     }
+    super.setSecondaryProgress(primary_progress + secondaryProgress);
+  }
 }
